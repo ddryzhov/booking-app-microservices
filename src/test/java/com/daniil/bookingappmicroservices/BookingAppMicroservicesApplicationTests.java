f8c527e8f212249dc9cd81e5d6e2e1f40c6f23b1
Daniil Dryzhov <danildryzov@gmail.com>
2026-02-03 18:00:50 +0100

    Initial commit: initialize Spring Boot project

---
+++ src/test/java/com/daniil/bookingappmicroservices/BookingAppMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniil.bookingappmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingAppMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
